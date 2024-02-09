3468e8fc62e466210063e57b3a21f872a8c3cb45
Ivanich41 <cooldude412003@gmail.com>
2024-02-09 14:54:17 +0300

    terraform part on ready

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-b"
+}
+
+resource "yandex_vpc_network" "yc-network" {
+  name = "vylnbox-network"
+}
+
+resource "yandex_vpc_subnet" "yc-subnet-0" {
+  name           = "yc-auto-subnet-0"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.yc-network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_compute_instance" "ad-vulnbox" {
+  name = "ad-vulnbox"
+  hostname = "ad-vulnbox"
+  platform_id = "standard-v1"
+  zone = "ru-central1-b"
+  resources {
+    cores  = 2
+    memory = 4
+  }
+  boot_disk {
+    disk_id = yandex_compute_disk.vulnbox-disk.id
+  }
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.yc-subnet-0.id}"
+    nat = true
+  }
+  metadata = {
+    user-data = "${file("./metadata.yml")}"
+  }
+  scheduling_policy {
+      preemptible = false
+    }
+}
+resource "yandex_compute_disk" "vulnbox-disk" {
+  name =  "vulnbox-disk"
+  size = 40
+  type = "network-ssd"
+  zone     = "ru-central1-b"
+  image_id = "fd8s78up10fbjbe5atn7" # Ubuntu 22.04
+}
